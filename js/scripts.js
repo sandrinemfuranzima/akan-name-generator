988c6e1df84926f03653f55c28d5c3007538617f
Nyota Mwangi <nyota.benjamin@gmail.com>
2019-11-30 16:19:59 +0300

    Add name arrays ordered by days from Sunday

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// The names listed below are ordered by days from Sunday
+var maleAkanNames = [
+  "Kwasi",
+  "Kwadwo",
+  "Kwabena",
+  "Kwaku",
+  "Yaw",
+  "Kofi",
+  "Kwame"
+];
+var femaleAkanNames = [
+  "Akosua",
+  "Adwoa",
+  "Abenaa",
+  "Akua",
+  "Yaa",
+  "Afua",
+  "Ama"
+];

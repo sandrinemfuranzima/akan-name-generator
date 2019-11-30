dd5a84f4ba7243cca45da0b02f1816a694dd0199
Nyota Mwangi <nyota.benjamin@gmail.com>
2019-11-30 19:00:21 +0300

    Initialize and invoke calcWeekDay function

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,22 @@ var femaleAkanNames = [
   "Afua",
   "Ama"
 ];
+
+/**
+ * Initialize and invoke calcWeekDay function.
+ * The function calculates the day of the week from a specific date.
+ * It returns the dayOfWeek as a zero-based index:: Sunday === 0;
+ */
+function calcWeekDay(month, day, year) {
+  var dayOfWeek;
+  var cc, yc;
+
+  // cc = centuryCode; yc = yearCode;
+  cc = parseInt(year / 100);
+  yc = year % 100;
+
+  dayOfWeek = (cc / 4 - 2 * cc - 2 + yc + yc / 4 + day + 2.6 * (month + 1)) % 7;
+  return dayOfWeek;
+}
+
+calcWeekDay(12, 16, 1997);
